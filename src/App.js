e40518dfbf058578aac1d61209e23bb4670ca96c
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2024-01-09 15:21:33 +0530

    include the sri lanka map component in app js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SriLankaMap from "./SriLankaMap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+return(
+  <div className="App">
+<header className="App-header">
+  <h1>Sri Lanka Map</h1>
+</header>
+<main>
+  <SriLankaMap />
+</main>
+  </div>
+);
 }
 
 export default App;
