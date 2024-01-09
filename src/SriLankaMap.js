46a689db9c812c393d8eca5c519e5a378850a04e
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2024-01-09 22:47:31 +0530

    style for the map

--- src/SriLankaMap.js
+++ src/SriLankaMap.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./srilanka.css";
 import sl_map from './svg/lk.svg'
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faMapMarker } from '@fortawesome/free-solid-svg-icons';
 
 const SriLankaMap = () => {
   return (
     <div className="map-container">
-      <img alt={"Sri Lankan Map"} src={sl_map} style={{width:"25%"}}/>
+      <img alt={"Sri Lankan Map"} src={sl_map} style={{width:"35%"}}/>
     </div>
   );
 };
