0071b7a3793cd9f9d88403a4f43f4233fa0f9448
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2024-01-11 14:22:01 +0530

    added hover into the map

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, {useState} from "react";
+import React from "react";
 import SriLankaMap from "./SriLankaMap";
-import {
-  ComposableMap,
-  Geographies,
-  Geography,
-  Marker,
-  Annotation,ZoomableGroup,
-} from "react-simple-maps";
-import ReactTooltip from "react-tooltip";
 
 function App() {
 return(
-  <div className="App" style={{width:"100%",height:"100%", display:"flex", flexDirection:"column", justifyContent:"center", alignItems:"center"}} >
+  <div className="App">
+<header className="App-header">
+
+</header>
+<main>
+  <SriLankaMap />
+</main>
   </div>
 );
 }
