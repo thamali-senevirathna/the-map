4dd04661cae48650a1f6600baf71cef2a64e523b
thamali-arunika <130566302+thamali-arunika@users.noreply.github.com>
2024-01-14 10:11:20 +0530

    style in world map

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
-import SriLankaMap from "./SriLankaMap";
+// import SriLankaMap from "./SriLankaMap";
+import WorldMap from "./WorldMap";
 
 function App() {
 return(
   <div className="App">
 <header className="App-header">
-<h1 style={{position:"absolute", top:"110%",left:"60%"}}>Sri Lanka</h1>
+{/* <h1 style={{position:"absolute", top:"110%",left:"60%"}}>Sri Lanka</h1> */}
 </header>
 <main>
-  <SriLankaMap />
+ <WorldMap />
 </main>
   </div>
 );
